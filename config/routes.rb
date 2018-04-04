bb5d408280f9dfd7e362cb7da133c3ddab0d4fdc
YVrush <yury.verush@gmail.com>
2018-04-04 05:15:58 -0400

    Revert "Assigngment complete"
    This reverts commit 1d6837ea9b73d08ff182d1c0e396875a56e82665.

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,6 @@ Rails.application.routes.draw do
 
   get 'welcome/about'
 
-  get 'welcome/contact'
-
   root 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
