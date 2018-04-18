decba34da0b213585728c75d59f74fd808060e74
YVrush <yury.verush@gmail.com>
2018-04-18 00:51:48 -0400

    no message

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,11 @@
 class PostsController < ApplicationController
   def index
     @posts = Post.all
-    @post.each_with_index do |post, index|
+    @posts.each_with_index do |post, index|
       if index % 5 == 0
         post.title = 'SPAM'
       end
-    end 
+    end
   end
 
   def show
