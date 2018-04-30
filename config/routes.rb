6c842d57c11417bf26bb75baafec166c51d3fd0e
YVrush <yury.verush@gmail.com>
2018-04-30 06:33:37 -0400

    Complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 
     resources :posts
+    resources :advertisements
 
     get 'about' => 'welcome#about'
 
-    root 'welcome#index'
+    root to: 'welcome#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180410012052) do
+ActiveRecord::Schema.define(version: 20180430094529) do
+
+  create_table "advertisements", force: :cascade do |t|
+    t.string "title"
+    t.text "copy"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "answers", force: :cascade do |t|
     t.text "body"
