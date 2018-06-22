621fc985c4d7c569dfd8f46fefc61e23c2781399
YVrush <yury.verush@gmail.com>
2018-06-21 23:31:35 -0400

    Stuck

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180430094529) do
+ActiveRecord::Schema.define(version: 20180620075505) do
 
   create_table "advertisements", force: :cascade do |t|
     t.string "title"
@@ -47,6 +47,14 @@ ActiveRecord::Schema.define(version: 20180430094529) do
     t.string "title"
     t.text "body"
     t.boolean "resolved"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string "name"
+    t.boolean "public", default: true
+    t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
