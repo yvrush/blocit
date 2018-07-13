a09e6e79539573696b400759db8fd5cf31e029bb
YVrush <yury.verush@gmail.com>
2018-07-13 19:17:11 -0400

    Complete Checkpoint 13JUL2018

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -8,9 +8,16 @@ RSpec.describe Post, type: :model do
  # #3
    let(:topic) { Topic.create!(name: name, description: description) }
  # #4
-   let(:post) { Topic.posts.create!(title: title, body: body) }
+   let(:post) { topic.posts.create!(title: title, body: body) }
 
    it { is_expected.to belong_to(:topic) }
+
+   it { is_expected.to validate_presence_of(:title) }
+   it { is_expected.to validate_presence_of(:body) }
+   it { is_expected.to validate_presence_of(:topic) }
+
+   it { is_expected.to validate_length_of(:title).is_at_least(5) }
+   it { is_expected.to validate_length_of(:body).is_at_least(20) }
 
    describe "attributes" do
      it "has title and body attributes" do
