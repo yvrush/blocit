bd4fa5753dc02d67e986253e74c04ca9f65a13dd
YVrush <yury.verush@gmail.com>
2018-07-21 19:48:29 -0400

    Assignment complete

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   before_save { self.email = email.downcase if email.present? }
+  before_save :format_name
 
       validates :name, length: { minimum: 1, maximum: 100 }, presence: true
       validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
@@ -9,4 +10,15 @@ class User < ApplicationRecord
                 uniqueness: { case_sensitive: false },
                 length: { minimum: 3, maximum: 254 }
       has_secure_password
+
+      def format_name
+          if name
+            name_array = [ ]
+            name.split.each do | name_part |
+              name_array << name_part.capitalize
+            end
+            self.name = name_array.join(" ")
+          end
+      end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,9 +18,17 @@ it { is_expected.to have_secure_password }
 it { is_expected.to validate_length_of(:password).is_at_least(6) }
 
 describe "attributes" do
-  it "should have name and email attributes" do
-    expect(user).to have_attributes(name: "Bloccit User", email: "user@bloccit.com")
-  end
+
+    it "should have name and email attributes" do
+      expect(user).to have_attributes(name: "Bloccit User", email: "user@bloccit.com")
+    end
+
+    it "should format user's name" do
+      user.name = "example user"
+      user.save
+      expect(user.name).to eq "Example User"
+    end
+
 end
 
 describe "invalid user" do
@@ -35,5 +43,5 @@ describe "invalid user" do
        expect(user_with_invalid_email).to_not be_valid
      end
    end
-   
+
 end
