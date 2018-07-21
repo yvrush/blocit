3b7bf1d4c7edbdda13c8e8b5904d61389562cfaf
YVrush <yury.verush@gmail.com>
2018-07-21 01:06:31 -0400

    Assignment-Validation-20JUL2018

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,8 @@
 class Topic < ApplicationRecord
   has_many :posts, dependent: :destroy
   has_many :sponsored_posts, dependent: :destroy
+
+  validates :name, length: { minimum: 5 }, presence: true
+  validates :description, length: { minimum: 15 }, presence: true
+
 end
--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -2,12 +2,20 @@ require 'rails_helper'
 
 
 RSpec.describe Topic, type: :model do
-  let(:name) { RandomData.random_sentence }
+   let(:name) { RandomData.random_sentence }
    let(:description) { RandomData.random_paragraph }
    let(:public) { true }
    let(:topic) { Topic.create!(name: name, description: description) }
 
    it { is_expected.to have_many(:posts) }
+
+   it { is_expected.to validate_presence_of(:name) }
+   it { is_expected.to validate_presence_of(:description) }
+
+   it { is_expected.to validate_length_of(:name).is_at_least(5) }
+   it { is_expected.to validate_length_of(:description).is_at_least(15) }
+
+
 
  # #1
    describe "attributes" do
