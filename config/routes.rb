13f6eb322484155c2c7f52576ffbc3e7c6d4254e
YVrush <yury.verush@gmail.com>
2018-07-23 22:49:10 -0400

    Assignment complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,27 @@
 Rails.application.routes.draw do
 
-  get 'sponsored_posts/show'
+    get 'sponsored_posts/show'
 
-  get 'sponsored_posts/new'
+    get 'sponsored_posts/new'
 
-  get 'sponsored_posts/edit'
+    get 'sponsored_posts/edit'
 
-  resources :advertisements
-  resources :questions
-  resources :topics do
-  resources :posts, except: [:index]
-  resources :sponsored_posts, except: [:index]
-
-  end
-  resources :users, only: [:new, :create] do
-    collection do
-      post 'confirm'
+    resources :advertisements
+    resources :questions
+    resources :topics do
+    resources :posts, except: [:index]
+    resources :sponsored_posts, except: [:index]
     end
-  end
+    
+    resources :users, only: [:new, :create] do
+      collection do
+        post 'confirm'
+      end
+    end
 
 
-  get 'about' => 'welcome#about'
+    get 'about' => 'welcome#about'
 
-  root to: 'welcome#index'
+    root to: 'welcome#index'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
